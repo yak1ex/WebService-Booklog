3cdff6fa2718f0b6808ddef9297a022715a82795
Yasutaka ATARASHI <yakex@cpan.org>
2013-04-17 07:47:12 +0900

    Implement get_review() and get_shelf() w/o tests.

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ version v0.0.0
 
 This module provides a way to access __UNOFFICIAL__ [booklog](http://booklog.jp) API.
 They are not only __UNOFFICIAL__ but also __UNDOCUMENTED__.
-Thus, it is expected to be quite __UNSTABLE__ and use with care.
+Thus, it is expected to be quite __UNSTABLE__. Please use with care.
 
 # METHODS
 
@@ -45,7 +45,7 @@ Available keys for `%params` are as follows:
 
     The number of items. Defaults to 5.
 
-Results are an object like the followings:
+Results are represented as an object like the followings:
 
     {
       tana => {
@@ -76,9 +76,224 @@ Results are an object like the followings:
 From the API, `[]`, empty array ref, denotes empty data.
 However, it is replaced as `{}`, empty hash ref, by this interface.
 
-This interface is not documented but it is used by public minishlef widget.
-Thus, it is expected to be a bit more stable than others.
+This interface is not documented but it is used by a public minishelf widget.
+Thus, it is expected to be a bit stabler than others.
 However, it is unofficial, too.
+
+## `get_review($book_id)`
+
+Get the review content of the specified `$book_id`. `$book_id` can be get by other interfaces.
+
+## `get_shelf($account, %arg)`
+
+Get shelf data of the specified $<$account>. Parameters are as follows:
+
+- `category`
+
+    Category ID dependent on user configuration.
+
+- `status`
+
+    One of `'want_read'`, `'reading'`, `'read'` and `'stacked'`. I believe the meanings are intuitive.
+
+- `rank`
+
+    An integer from 1 to 5 inclusive.
+
+- `sort`
+
+    A string matching `(release|date|read|title)_(desc|asc)`. `date` means register date.
+
+- `tag`
+
+    Tag string
+
+- `keyword`
+
+    Keyword string
+
+- `page`
+
+    Page number
+
+- `parpage`
+
+    Items par page.
+
+- `genre`
+
+    Any of `'other'`, `'book'`, `'ebook'` (e-book), `'comic'`, `'fbook'` (foreign book), `'magazine'`, `'movie'`, `'music'` and `'game'`.
+
+Results are represented as an object like the followings:
+
+    {"user":
+        {"user_id":"<id>",
+         "account":"<account>",
+         "plan_id":"0",
+         "nickname":"<nick>",
+         "image_url":"<url>",
+         "associateid":"",
+         "last_login":"yyyy-mm-dd hh:mm:ss",
+         "create_on":"yyyy-mm-dd hh:mm:ss"}
+        },
+     "login":
+         {"user_id":"<id>",
+          "account":"<account>",
+          "shelf_id":"<shelf_id>",
+          "plan_id":"0"
+         },
+     "genre_id":null,
+     "category_id":"0",
+     "status":"0",
+     "rank":"0",
+     "tag":null,
+     "keyword":null,
+     "sort":"sort_desc",
+     "books":[
+       {"book_id":"<id>",
+        "service_id":"1", // amazon.co.jp
+        "id":"<asin>",
+        "rank":"0",
+        "category_id":"0",
+        "public":"1",
+        "status":"0",
+        "create_on":"yyyy-mm-dd hh:mm:ss",
+        "read_at":"yyyy\u5e74mm\u6708dd\u65e5", // or null
+        "title":"<title>",
+        "title2":"<title2>",
+        "image":"<url>",
+        "height":<height>,
+        "width":<width>,
+        "item":
+            {"service_id":1, // amazon.co.jp
+             "id":"<asin>",
+             "url":"<url>",
+             "title":"<title>",
+             "authors":["<author>"],
+             "directors":[],
+             "artists":[],
+             "actors":[],
+             "creators":["<illustrator>"],
+             "small_image_url":"<url>",
+             "small_image_width":"<width>",
+             "small_image_height":"<height>",
+             "medium_image_url":"<url>",
+             "medium_image_width":"<width>",
+             "medium_image_height":"<height>",
+             "large_image_url":"<url>",
+             "large_image_width":"<width>",
+             "large_image_height":"<height>",
+             "publisher":"<publisher>",
+             "release_date":"yyyy-mm-dd",
+             "genre_id":1,
+             "price":"\uffe5 <price>",
+             "savedPrice":"\uffe5 <price>",
+             "EAN":"<EAN>",
+             "languages":[],
+             "pages":"<pages>",
+             "ProductGroup":"<Group>",
+             "Binding":"\u6587\u5eab",
+             "platform":"",
+             "AlternateVersions":[],
+             "isAdult":"0",
+             "create_on":1365502041
+             },
+         "tags":[],
+         "quotes":[]
+       },
+     ],
+     "reviews":false, // { "<id>":{"more":true,"public":"1","description":""}, ... }
+     "comments":[],
+     "pager":
+         {"base_url":"<url>",
+          "query":"",
+          "total":"305",
+          "start":1,
+          "end":25,
+          "parpage":"25",
+          "page":1,
+          "maxpage":13,
+          "startpage":1,
+          "lastpage":10,
+          "prevpage":0,
+          "nextpage":2}
+       }
+    }
+
+Probably, structure of `$result->{books}[n]{item}` depends on serivice provider.
+I did not and will not investigate them deeply. If you have some information, please let me know.
+
+- 1
+
+    Amazon.co.jp
+
+- 2
+
+    Amazon.com
+
+- 3
+
+    パブー
+
+- 4
+
+    iTunes Store
+
+- 5
+
+    unknown
+
+- 6
+
+    unknown
+
+- 7
+
+    青空文庫
+
+- 8
+
+    BookLive!
+
+- 9
+
+    GALAPAGOS
+
+- 10
+
+    達人出版会
+
+- 11
+
+    O'Reilly Japan
+
+- 12
+
+    unknown
+
+- 13
+
+    技術評論社
+
+- 14
+
+    unknown
+
+- 15
+
+    パブリ
+
+- 16
+
+    honto
+
+- 17
+
+    BOOK☆WALKER
+
+- 18
+
+    ニコニコ静画
 
 # SEE ALSO
 
--- lib/WebService/Booklog.pm
+++ lib/WebService/Booklog.pm
@@ -35,10 +35,32 @@ sub get_minishelf_data
 	}
 	my $param = join '&', map { $_.'='.$data->{$_} } keys %$data;
 	$param = "?$param" if length $param;
-	my $ret = JSON::Any->from_json($self->{_UA}->post('http://api.booklog.jp/json/'.$account.$param)->content);
+	my $ret = JSON::Any->from_json($self->{_UA}->get('http://api.booklog.jp/json/'.$account.$param)->content);
 	$ret->{category} = {} if ref $ret->{category} eq 'ARRAY';
 	$ret->{books} = [ grep { ref $_ ne 'ARRAY' } @{$ret->{books}} ];
 	return $ret;
+}
+
+sub get_review
+{
+	my ($self, $book_id) = @_;
+
+	my $ret = JSON::Any->from_json($self->{_UA}->get('http://booklog.jp/json/review/'.$book_id)->content);
+	return $ret->{$book_id} if exists $ret->{$book_id};
+	return;
+}
+
+sub get_shelf
+{
+	my ($self, $account, %arg) = @_;
+	my $data = {};
+	$data->{status} = $status{lc $arg{status}} if exists $arg{status};
+	$data->{category_id} = $arg{category} if exists $arg{category};
+	foreach my $key (qw(sort rank tag genre page parpage keyword)) {
+		$data->{$key} = $arg{$key} if exists $arg{$key};
+	}
+	my $param = join '&', map { $_.'='.$data->{$_} } keys %$data;
+	my $ret = JSON::Any->from_json($self->{_UA}->get('http://booklog.jp/users_api/'.$account.$param)->content);
 }
 
 1;
@@ -65,141 +87,7 @@ booklist[]=numeric_id&booklist[]=numeric_id&...
     var url ='/api/follow/add';
     var data = 'account=' + account;
 
-    var url = "/json/review/" + book_id;
-{book_id:""}
-
-                parpage     = 25,
-                account     = $obj.data('account'),
-                genre       = $obj.data('genre') || 'all',
-                category_id = $obj.data('category-id'),
-                status      = $obj.data('status'),
-                sort        = $obj.data('sort'),
-                rank        = $obj.data('rank'),
-                tag         = $obj.data('tag'),
-                keyword     = $obj.data('keyword'),
-                url = '/users_api/' + account,
-                params = {
-                    'category_id': category_id,
-                    'status': status,
-                    'sort': sort, // (release|date|read|title)_(desc|asc)
-                    'rank': rank,
-                    'tag': tag,
-                    'page': page,
-                    'genre': genre, // 1 => book, 3 => comic, 4 => fbook, 6 => movie, 5 => magazine, 2 => ebook, 7 => music, 8 => game, 0 => other
-                    'parpage': parpage
-                },
-                result, i, max, user, login, books, reviews;
-
-<select name="service_id" class="service_id inputSelect">
-    <option value="1" selected="selected">Amazon.co.jp</option>
-    <option value="2">Amazon.com</option>
-    <option value="3">ƒpƒu[</option>
-    <option value="4">iTunes Store</option>
-    <option value="7">Â‹ó•¶ŒÉ</option>
-    <option value="8">BookLive!</option>
-    <option value="9">GALAPAGOS</option>
-    <option value="10">’Blo”Å‰ï</option>
-    <option value="11">O'Reilly Japan</option>
-    <option value="13">‹Zp•]˜_Ğ</option>
-    <option value="15">ƒpƒuƒŠ</option>
-    <option value="16">honto</option>
-    <option value="17">BOOK™WALKER</option>
-    <option value="19">ƒjƒRƒjƒRÃ‰æ</option>
-
 /users_api/<account>
-{"user":
-    {"user_id":"<id>",
-     "account":"<account>",
-     "plan_id":"0",
-     "nickname":"<nick>",
-     "image_url":"<url>",
-     "associateid":"",
-     "last_login":"yyyy-mm-dd hh:mm:ss",
-     "create_on":"yyyy-mm-dd hh:mm:ss"}
-    },
- "login":
-     {"user_id":"<id>",
-      "account":"<account>",
-      "shelf_id":"<shelf_id>",
-      "plan_id":"0"
-     },
- "genre_id":null,
- "category_id":"0",
- "status":"0",
- "rank":"0",
- "tag":null,
- "keyword":null,
- "sort":"sort_desc",
- "books":[
-   {"book_id":"<id>",
-    "service_id":"1", // might be amazon?
-    "id":"<asin>",
-    "rank":"0",
-    "category_id":"0",
-    "public":"1",
-    "status":"0",
-    "create_on":"yyyy-mm-dd hh:mm:ss",
-    "read_at":"yyyy\u5e74mm\u6708dd\u65e5", // or null
-    "title":"<title>",
-    "title2":"<title2>",
-    "image":"<url>",
-    "height":<height>,
-    "width":<width>,
-    "item":
-        {"service_id":1, // amazon?
-         "id":"<asin>",
-         "url":"<url>",
-         "title":"<title>",
-         "authors":["<author>"],
-         "directors":[],
-         "artists":[],
-         "actors":[],
-         "creators":["<illustrator>"],
-         "small_image_url":"<url>",
-         "small_image_width":"<width>",
-         "small_image_height":"<height>",
-         "medium_image_url":"<url>",
-         "medium_image_width":"<width>",
-         "medium_image_height":"<height>",
-         "large_image_url":"<url>",
-         "large_image_width":"<width>",
-         "large_image_height":"<height>",
-         "publisher":"<publisher>",
-         "release_date":"yyyy-mm-dd",
-         "genre_id":1,
-         "price":"\uffe5 <price>",
-         "savedPrice":"\uffe5 <price>",
-         "EAN":"<EAN>",
-         "languages":[],
-         "pages":"<pages>",
-         "ProductGroup":"<Group>",
-         "Binding":"\u6587\u5eab",
-         "platform":"",
-         "AlternateVersions":[],
-         "isAdult":"0",
-         "create_on":1365502041
-         },
-     "tags":[],
-     "quotes":[]
-  },
- ],
- "reviews":false, // { "<id>":{"more":true,"public":"1","description":""}, ... }
- "comments":[],
- "pager":
-     {"base_url":"<url>",
-      "query":"",
-      "total":"305",
-      "start":1,
-      "end":25,
-      "parpage":"25",
-      "page":1,
-      "maxpage":13,
-      "startpage":1,
-      "lastpage":10,
-      "prevpage":0,
-      "nextpage":2}
-     }
-}
 
   <form action="/edit/1/4864683212" method="post">
   <input type="hidden" name="_method" value="edit" />
@@ -207,23 +95,23 @@ booklist[]=numeric_id&booklist[]=numeric_id&...
             <select name="read_at_y" id="read_at_y">
                             <option value="3" selected="selected">3</option>
             <select name="read_at_m" id="read_at_m">
-              <option value="0">‚È‚µ</option>
+              <option value="0">ãªã—</option>
             <select name="read_at_d" id="read_at_d">
                             <option value="12" selected="selected">12</option>
           <input id="read_at_null" type="checkbox" name="read_at_null" value="0" onClick="changeReadAt(this)" />
-        <span>“Ç—¹“ú‚ğw’è‚µ‚È‚¢</span>
+        <span>èª­äº†æ—¥ã‚’æŒ‡å®šã—ãªã„</span>
 	<select name="rank" class="inputselect">
 	<select id="category_edit" name="category_id" class="inputselect">
       <textarea id="review" name="description" class="inputarea" style="height:180px;">
-<input type="checkbox" id="public" name="public" value="2" /><label for="public">ƒlƒ^ƒoƒŒ‚Ì“à—e‚ğŠÜ‚Ş</label>
+<input type="checkbox" id="public" name="public" value="2" /><label for="public">ãƒã‚¿ãƒãƒ¬ã®å†…å®¹ã‚’å«ã‚€</label>
       <input type="text" id="tags" name="tags" value="" class="inputtext" />    
-    <input type="text" id="create_on_y" name="create_on_y" value="2013" maxlength="4" class="inputtext" style="width:40px;" />”N
-    <input type="text" id="create_on_m" name="create_on_m" value="03" maxlength="2" class="inputtext" style="width:20px;" />Œ
-    <input type="text" id="create_on_d" name="create_on_d" value="12" maxlength="2" class="inputtext" style="width:20px;" />“ú
+    <input type="text" id="create_on_y" name="create_on_y" value="2013" maxlength="4" class="inputtext" style="width:40px;" />å¹´
+    <input type="text" id="create_on_m" name="create_on_m" value="03" maxlength="2" class="inputtext" style="width:20px;" />æœˆ
+    <input type="text" id="create_on_d" name="create_on_d" value="12" maxlength="2" class="inputtext" style="width:20px;" />æ—¥
     &nbsp;
-    <input type="text" id="create_on_h" name="create_on_h" value="22" maxlength="2" class="inputtext" style="width:20px;" />
-    <input type="text" id="create_on_i" name="create_on_i" value="49" maxlength="2" class="inputtext" style="width:20px;" />•ª
-    <input type="text" id="create_on_s" name="create_on_s" value="17" maxlength="2" class="inputtext" style="width:20px;" />•b
+    <input type="text" id="create_on_h" name="create_on_h" value="22" maxlength="2" class="inputtext" style="width:20px;" />æ™‚
+    <input type="text" id="create_on_i" name="create_on_i" value="49" maxlength="2" class="inputtext" style="width:20px;" />åˆ†
+    <input type="text" id="create_on_s" name="create_on_s" value="17" maxlength="2" class="inputtext" style="width:20px;" />ç§’
   <textarea id="memo" name="memo" class="inputarea" style="height:50px;"></textarea>    
 
   <form action="/edit/1/4864683212#select_tab2" method="post" id="form_quote">
@@ -274,7 +162,7 @@ An integer from 1 to 5 inclusive.
 = C<count>
 The number of items. Defaults to 5.
 
-Results are an object like the followings:
+Results are represented as an object like the followings:
 
   {
     tana => {
@@ -308,6 +196,153 @@ However, it is replaced as C<{}>, empty hash ref, by this interface.
 This interface is not documented but it is used by a public minishelf widget.
 Thus, it is expected to be a bit stabler than others.
 However, it is unofficial, too.
+
+=method C<get_review($book_id)>
+
+Get the review content of the specified C<$book_id>. C<$book_id> can be get by other interfaces.
+
+=method C<get_shelf($account, %arg)>
+
+Get shelf data of the specified $<$account>. Parameters are as follows:
+
+=for :list
+= C<category>
+Category ID dependent on user configuration.
+= C<status>
+One of C<'want_read'>, C<'reading'>, C<'read'> and C<'stacked'>. I believe the meanings are intuitive.
+= C<rank>
+An integer from 1 to 5 inclusive.
+= C<sort>
+A string matching C<(release|date|read|title)_(desc|asc)>. C<date> means register date.
+= C<tag>
+Tag string
+= C<keyword>
+Keyword string
+= C<page>
+Page number
+= C<parpage>
+Items par page.
+= C<genre>
+Any of C<'other'>, C<'book'>, C<'ebook'> (e-book), C<'comic'>, C<'fbook'> (foreign book), C<'magazine'>, C<'movie'>, C<'music'> and C<'game'>.
+
+Results are represented as an object like the followings:
+
+  {"user":
+      {"user_id":"<id>",
+       "account":"<account>",
+       "plan_id":"0",
+       "nickname":"<nick>",
+       "image_url":"<url>",
+       "associateid":"",
+       "last_login":"yyyy-mm-dd hh:mm:ss",
+       "create_on":"yyyy-mm-dd hh:mm:ss"}
+      },
+   "login":
+       {"user_id":"<id>",
+        "account":"<account>",
+        "shelf_id":"<shelf_id>",
+        "plan_id":"0"
+       },
+   "genre_id":null,
+   "category_id":"0",
+   "status":"0",
+   "rank":"0",
+   "tag":null,
+   "keyword":null,
+   "sort":"sort_desc",
+   "books":[
+     {"book_id":"<id>",
+      "service_id":"1", // amazon.co.jp
+      "id":"<asin>",
+      "rank":"0",
+      "category_id":"0",
+      "public":"1",
+      "status":"0",
+      "create_on":"yyyy-mm-dd hh:mm:ss",
+      "read_at":"yyyy\u5e74mm\u6708dd\u65e5", // or null
+      "title":"<title>",
+      "title2":"<title2>",
+      "image":"<url>",
+      "height":<height>,
+      "width":<width>,
+      "item":
+          {"service_id":1, // amazon.co.jp
+           "id":"<asin>",
+           "url":"<url>",
+           "title":"<title>",
+           "authors":["<author>"],
+           "directors":[],
+           "artists":[],
+           "actors":[],
+           "creators":["<illustrator>"],
+           "small_image_url":"<url>",
+           "small_image_width":"<width>",
+           "small_image_height":"<height>",
+           "medium_image_url":"<url>",
+           "medium_image_width":"<width>",
+           "medium_image_height":"<height>",
+           "large_image_url":"<url>",
+           "large_image_width":"<width>",
+           "large_image_height":"<height>",
+           "publisher":"<publisher>",
+           "release_date":"yyyy-mm-dd",
+           "genre_id":1,
+           "price":"\uffe5 <price>",
+           "savedPrice":"\uffe5 <price>",
+           "EAN":"<EAN>",
+           "languages":[],
+           "pages":"<pages>",
+           "ProductGroup":"<Group>",
+           "Binding":"\u6587\u5eab",
+           "platform":"",
+           "AlternateVersions":[],
+           "isAdult":"0",
+           "create_on":1365502041
+           },
+       "tags":[],
+       "quotes":[]
+     },
+   ],
+   "reviews":false, // { "<id>":{"more":true,"public":"1","description":""}, ... }
+   "comments":[],
+   "pager":
+       {"base_url":"<url>",
+        "query":"",
+        "total":"305",
+        "start":1,
+        "end":25,
+        "parpage":"25",
+        "page":1,
+        "maxpage":13,
+        "startpage":1,
+        "lastpage":10,
+        "prevpage":0,
+        "nextpage":2}
+     }
+  }
+
+Probably, structure of C<$result-E<gt>{books}[n]{item}> depends on serivice provider.
+I did not and will not investigate them deeply. If you have some information, please let me know.
+
+=for :list
+1. Amazon.co.jp
+2. Amazon.com
+3. ãƒ‘ãƒ–ãƒ¼
+4. iTunes Store
+5. unknown
+6. unknown
+7. é’ç©ºæ–‡åº«
+8. BookLive!
+9. GALAPAGOS
+10. é”äººå‡ºç‰ˆä¼š
+11. O'Reilly Japan
+12. unknown
+13. æŠ€è¡“è©•è«–ç¤¾
+14. unknown
+15. ãƒ‘ãƒ–ãƒª
+16. honto
+17. BOOKâ˜†WALKER
+18. ãƒ‹ã‚³ãƒ‹ã‚³é™ç”»
 
 =head1 SEE ALSO
 
